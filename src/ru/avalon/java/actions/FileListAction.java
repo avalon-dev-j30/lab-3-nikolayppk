dec3ee28f63de67d6cd6139dcb4cfd90cf6742c2
LuschenkoNM <LuschenkoNM@NSC181380.nsc.ru>
2019-06-20 12:10:41 +0300

    J30_Lab3 - Done

---
+++ src/ru/avalon/java/actions/FileListAction.java
@@ -0,0 +1,27 @@
+package ru.avalon.java.actions;
+
+import java.io.File;
+
+public class FileListAction implements Action  {
+
+    private File file;
+    public FileListAction(String from) {
+        this.file = new File(from);
+    }
+
+    @Override
+    public void run() {
+        
+        if(file.exists()){
+            for (File fileEntry : file.listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    System.out.println("/"+fileEntry.getName());
+                } else {
+                    System.out.println(fileEntry.getName());
+                }
+            }
+        }else{
+            System.out.printf("папка %s не найдена", file.toString());
+        }
+    }
+}
